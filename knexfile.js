1a87b165102dbcfb6a4113f8552c557fa6d22b33
Colby Allen <colbyallen012@gmail.com>
2019-12-09 15:58:41 -0700

    update knex file

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/vinos',
+    connection: 'postgres://localhost/wines',
     migrations: {
       directory: './db/migrations'
     },
