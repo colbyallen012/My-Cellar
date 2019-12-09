f3c6fe24661f03efe9efc124dbf53177968f8c32
Colby Allen <colbyallen012@gmail.com>
2019-12-09 14:37:51 -0700

    add querystring

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
+const querystring = require('querystring');
 
 app.use(cors());
 app.use(express.json());
