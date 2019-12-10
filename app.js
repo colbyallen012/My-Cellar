419069a50d2a05d446536a23d04cee49d2e2d94d
Colby Allen <colbyallen012@gmail.com>
2019-12-10 10:00:39 -0700

    uncomment app, bug fixed

--- app.js
+++ app.js
@@ -12,45 +12,44 @@ app.use(express.static('Public'));
 app.locals.title = 'My Cellar';
 
 app.get('/', (request, response) => {
-  // response.send('We\'re gonna do it!');
 });
 
-// app.get('/api/v1/vinos', (request, response) => {
-//   database('vinos').select()
-//     .then(vinos => response.status(200).json(vinos))
-//     .catch(error => response.status(500).json({error}))
-// });
+app.get('/api/v1/vinos', (request, response) => {
+  database('vinos').select()
+    .then(vinos => response.status(200).json(vinos))
+    .catch(error => response.status(500).json({error}))
+});
 
-// app.post('/api/v1/vinos', (request, response) => {
-//   const wines = request.body;
-//   for(let requiredParameter of ['vineyard', 'name', 'color', 'type', 'year', 'rating']) {
-//     if(!wines[requiredParameter]) {
-//       return response
-//         .status(422)
-//         .send({error: `Expected format: { vineyard: <String>, name: <String>, color: <String>, type: <String>, year: <Integer>, rating: <Integer>}. You're missing the ${requiredParameter} property.`})
-//     }
-//   }
-//   database('vinos').insert(wines, 'id')
-//     .then(wines => {
-//       response.status(201).json({ id: wines[0] })
-//     })
-//     .catch(error => {
-//       response.status(500).json({ error });
-//     });
-// });
+app.post('/api/v1/vinos', (request, response) => {
+  const wines = request.body;
+  for(let requiredParameter of ['vineyard', 'name', 'color', 'type', 'year', 'rating']) {
+    if(!wines[requiredParameter]) {
+      return response
+        .status(422)
+        .send({error: `Expected format: { vineyard: <String>, name: <String>, color: <String>, type: <String>, year: <Integer>, rating: <Integer>}. You're missing the ${requiredParameter} property.`})
+    }
+  }
+  database('vinos').insert(wines, 'id')
+    .then(wines => {
+      response.status(201).json({ id: wines[0] })
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
 
-// app.delete('/api/v1/vinos/:id', (request, response) => {
-//   const {id} = request.params;
-//   database('vinos').where('id', id).select()
-//     .then(wines => {
-//       if(wines.length) {
-//             database('vinos').where('id', id).del()
-//               .then(() => response.status(204).json('Wine deleted'))
-//               .catch(error => response.status(500).json({error}))
-//       } else {
-//         response.status(404).json('Wine does not exist')
-//       }
-//     })
-// })
+app.delete('/api/v1/vinos/:id', (request, response) => {
+  const {id} = request.params;
+  database('vinos').where('id', id).select()
+    .then(wines => {
+      if(wines.length) {
+            database('vinos').where('id', id).del()
+              .then(() => response.status(204).json('Wine deleted'))
+              .catch(error => response.status(500).json({error}))
+      } else {
+        response.status(404).json('Wine does not exist')
+      }
+    })
+})
 
 module.exports = app;
